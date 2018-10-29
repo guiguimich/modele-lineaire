f79f51ff48e0d952908cd5259f2e22a5154685cb
Olivier Turcotte <olivier.turcotte.18@ulaval.ca>
2018-10-29 16:54:01 -0400

    Working on the model

--- TP1/script.R
+++ TP1/script.R
@@ -4,19 +4,9 @@ pacman::p_load(ggplot2,gridExtra,MASS,car)
 
 # Analyse préliminaire des données ----
 data <- read.csv("AutoBodyInjury.csv",sep = ";")
-tail(sort(data$CLMAGE),5) 
-tail(sort(data$LOSS),5)
-str(data)
+
 summary(data[,7:8])
 ## Correction des erreurs dans MARITAL
-boxplot(data$LOSS)
-p7 <- ggplot(data, aes(x = Month, y = Ozone)) +
-    geom_boxplot()
-par(mfrow = c(1,2))
-t <- head(sort(data$LOSS),-1)
-boxplot(t)
-hist(log(t))
-hist(log(data$LOSS))
 correction <- gsub("ma.*","married",data$MARITAL)
 correction <- gsub("si.*","single",correction)
 data$MARITAL <- factor(correction)
@@ -25,11 +15,14 @@ data$MARITAL <- factor(correction)
 correction <- gsub("male*","M",data$CLMSEX)
 data$CLMSEX <- factor(correction)
 
+## Correction des erreurs dans CLMAGE
+data <- data[-66,]
 
 # Conversion en factor de ATTORNEY,CLIMINSUR & SEATBELT
 for(i in c("ATTORNEY","CLMINSUR","SEATBELT")){
     data[[i]] <- as.factor(data[[i]])
 }
+
 #sapply(data,class)
 
 # Création du plot des fréquences ----
@@ -51,66 +44,39 @@ grid.arrange(p1,p2,p3,p4,p5, nrow = 2)
 
 # Modèle ----
 summary(data$LOSS)
+bcox <- lm(LOSS ~ CLMAGE,data = data) # On a omis les variables qualitatives pour en faire un modèle simlpe
+# avec les variables qualitatives incluts dans B0
+boxcox(bcox) # Boxcox indique lambda = 0 à 95% -> la transformation a Y à faire est log(Y)
 
-boxplot(data$CLMAGE)# un age de 610 et des ages de 0 : peut être problématique
-boxplot(data$LOSS) # Loss de 1m très éloignée des autres, va être à regarder
-hist(log(1+data$CLMAGE)) # Un log(1+age)(car il y a des valeurs de 0) pourrait être utilisé ,car fortement asymétrique
-hist(log(data$LOSS)) ## Un log(1+loss) pourrait être utilisé ,car fortement asymétrique
+modele1 <- lm(I(log(LOSS)) ~ CLMAGE + ATTORNEY  + SEATBELT + MARITAL + CLMSEX + CLMINSUR,data = data)
+par(mfrow = c(1,1))
+plot(data$CLMAGE,rstudent(modele1),xlab="Me lost",main="Please, mean = 0 ")
+summary(modele1)
+vif(modele1)
+AIC_m1 <- stepAIC(modele1, direction="both")
+modele1_mod <- lm(formula(AIC_m1),data = data)
+summary(modele1_mod)
+plot(data$CLMAGE,rstudent(modele1_mod),xlab="CLMAGE",main="Résidus studentisé en fonction de CLMAGE")
+qqnorm(rstudent(modele1_mod))
+qqline(as.numeric(rstudent(modele1_mod)))
 
-## Voici les 4 principaux modèles comportants toutes les interactions
-(fit1 <- lm(I(log(LOSS))~I(log(1+CLMAGE)) + ATTORNEY + CLMSEX + MARITAL + CLMINSUR + SEATBELT 
-
-                + I(log(1+CLMAGE))*ATTORNEY + I(log(1+CLMAGE))*CLMSEX + I(log(1+CLMAGE))*MARITAL + I(log(1+CLMAGE))*CLMINSUR + I(log(1+CLMAGE))*SEATBELT
-                + ATTORNEY*CLMSEX + ATTORNEY*MARITAL + ATTORNEY*CLMINSUR + ATTORNEY*SEATBELT
-                + CLMSEX*MARITAL + CLMSEX*CLMINSUR + CLMSEX*SEATBELT
-                + MARITAL*CLMINSUR + MARITAL*SEATBELT
-                +CLMINSUR*SEATBELT,data=data))
-(fit2 <- lm(LOSS~I(log(1+CLMAGE)) + ATTORNEY + CLMSEX + MARITAL + CLMINSUR + SEATBELT 
-            + I(log(1+CLMAGE))*ATTORNEY + I(log(1+CLMAGE))*CLMSEX + I(log(1+CLMAGE))*MARITAL + I(log(1+CLMAGE))*CLMINSUR + I(log(1+CLMAGE))*SEATBELT
-            + ATTORNEY*CLMSEX + ATTORNEY*MARITAL + ATTORNEY*CLMINSUR + ATTORNEY*SEATBELT
-            + CLMSEX*MARITAL + CLMSEX*CLMINSUR + CLMSEX*SEATBELT
-            + MARITAL*CLMINSUR + MARITAL*SEATBELT
-            +CLMINSUR*SEATBELT,data=data))
-(fit3 <- lm(LOSS~CLMAGE + ATTORNEY + CLMSEX + MARITAL + CLMINSUR + SEATBELT 
-            + CLMAGE*ATTORNEY + CLMAGE*CLMSEX + CLMAGE*MARITAL + CLMAGE*CLMINSUR + CLMAGE*SEATBELT
-            + ATTORNEY*CLMSEX + ATTORNEY*MARITAL + ATTORNEY*CLMINSUR + ATTORNEY*SEATBELT
-            + CLMSEX*MARITAL + CLMSEX*CLMINSUR + CLMSEX*SEATBELT
-            + MARITAL*CLMINSUR + MARITAL*SEATBELT
-            +CLMINSUR*SEATBELT,data=data))
-(fit4 <- lm(log(LOSS)~CLMAGE + ATTORNEY + CLMSEX + MARITAL + CLMINSUR + SEATBELT 
-            + CLMAGE*ATTORNEY + CLMAGE*CLMSEX + CLMAGE*MARITAL + CLMAGE*CLMINSUR + CLMAGE*SEATBELT
-            + ATTORNEY*CLMSEX + ATTORNEY*MARITAL + ATTORNEY*CLMINSUR + ATTORNEY*SEATBELT
-            + CLMSEX*MARITAL + CLMSEX*CLMINSUR + CLMSEX*SEATBELT
-            + MARITAL*CLMINSUR + MARITAL*SEATBELT
-            +CLMINSUR*SEATBELT,data=data))
+modele2 <- lm(log(LOSS) ~ CLMAGE + ATTORNEY + MARITAL + SEATBELT + CLMAGE:ATTORNEY,data = data)
+par(mfrow = c(1,1))
+summary(modele2)
+vif(modele2)
+AIC_m2 <- stepAIC(modele2, direction="both")
+modele2_mod <- lm(formula(AIC_m2),data = data)
+summary(modele2_mod)
+plot(data$CLMAGE,rstudent(modele2_mod),xlab="CLMAGE",main="Résidus studentisé en fonction de CLMAGE")
+qqnorm(rstudent(modele2_mod))
+qqline(as.numeric(rstudent(modele2_mod)))
 
 
-
-#l'interaction MARITAL*SEATBELT semble ne pas fonctionner
-## Mon idée: faire la méthode forward ou backward sur le modèle complet plus haut pour obtenir le
-## bon modèle à utiliser 
-## On remarque que tant et aussi longtemps qu'on a des interactions, il y a multicolinéarité 
-## donc le modèle semble ne pas avoir d'interactions
-
-
-step1 <- stepAIC(fit1, direction="both")
-step2 <- stepAIC(fit1, direction="forward")
-step3 <- stepAIC(fit1, direction="backward")
-debut_fit <- lm(I(log(LOSS)) ~ I(log(1 + CLMAGE)) + ATTORNEY + MARITAL + CLMINSUR + 
-                  SEATBELT ,data=data) 
-
-step1 <- stepAIC(debut_fit, direction="both")
-step2 <- stepAIC(debut_fit, direction="forward")
-step3 <- stepAIC(debut_fit, direction="backward")
-
-test1 <- lm(formula(step1),data=data)
-test2 <- lm(formula(step2),data=data)
-
-summary(test1)
-summary(test2)### regarder si ca vaut la peine de garder CLMINSUR
-
-
-vif(good_fit1) # meilleur modèle pour les VIFS
-# les interactions causent des problèmes de VIFs
-
-
+verif <- function(modele){
+    par(mfrow = c(1,2))
+    plot(data$CLMAGE,rstudent(modele),xlab="CLMAGE",main="Student")
+    qqnorm(rstudent(modele))
+    qqline(as.numeric(rstudent(modele)))
+    summary(modele)
+}
+verif(lm(log(LOSS) ~ CLMAGE + ATTORNEY + MARITAL + SEATBELT + CLMAGE:ATTORNEY,data = data))
